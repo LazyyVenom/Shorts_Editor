16cfb11bef2c8528cf5b656bfc439e4c477b02ce
Anubhav Choubey <choubey.anubhav253@gmail.com>
2024-12-20 09:16:48 +0530

    making the text look better

--- utils/add_captions.py
+++ utils/add_captions.py
@@ -6,9 +6,15 @@ from moviepy.video.fx import Painting
 video = VideoFileClip(filename="input_video.mp4")
 
 text = TextClip(
-    font="static\Coolvetica Rg.otf", text="Your Text Here", font_size=50, color="white",duration=video.duration
+    font="static\Coolvetica Rg.otf",
+    text="Your Text Here",
+    font_size=50,
+    color="white",
+    duration=video.duration,
+    text_align="center",
+    size=(video.w, video.h),
+    method="caption",
 )
-
 video_with_text = CompositeVideoClip([video, text])
 
 video_with_text.write_videofile("output_video.mp4", codec="libx264", fps=video.fps)
@@ -16,7 +22,7 @@ video_with_text.write_videofile("output_video.mp4", codec="libx264", fps=video.f
 
 # This is gonna take some time as the code is updated and chat gpt don't gets the updated one
 
-#OK IT IS TAKING TIME TO PROCESS THE VIDEO  
+# OK IT IS TAKING TIME TO PROCESS THE VIDEO
 # GOOD THING - We can set the time duration for texts easily Let's Go
 
-#NGL Text looking good need to make some good functions and A class to make it usable
+# NGL Text looking good need to make some good functions and A class to make it usable
