d3f3b9a0ba7b9174d04492834ae603e381d8e5ef
Anubhav Choubey <choubey.anubhav253@gmail.com>
2024-12-20 09:20:09 +0530

    Making it oops based now

--- utils/add_captions.py
+++ utils/add_captions.py
@@ -2,6 +2,22 @@
 
 from moviepy import VideoFileClip, TextClip, CompositeVideoClip
 from moviepy.video.fx import Painting
+from typing import List
+
+
+class AddCaptions:
+    def __init__(
+        self,
+        video_path: str,
+        texts: List[str],
+        font_size: int = 50,
+        font: str = "static\Coolvetica Rg.otf",
+    ):
+        self.video = VideoFileClip(filename=video_path)
+        self.font = font
+        self.font_size = font_size
+        self.texts = texts
+
 
 video = VideoFileClip(filename="input_video.mp4")
 
@@ -19,6 +35,8 @@ video_with_text = CompositeVideoClip([video, text])
 
 video_with_text.write_videofile("output_video.mp4", codec="libx264", fps=video.fps)
 
+
+# Simplifying the function so that it can be used easily
 
 # This is gonna take some time as the code is updated and chat gpt don't gets the updated one
 
