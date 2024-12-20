8ecfccd401bf6b14253d2e42ba1af70f20d39b3b
Anubhav Choubey <choubey.anubhav253@gmail.com>
2024-12-20 09:46:50 +0530

    Basic basic starting

--- utils/audio_recognition.py
+++ utils/audio_recognition.py
@@ -1,2 +1,20 @@
-# NEED to put audio_recognition part here
+import speech_recognition as sr
 
+# NEED to put audio_recognition part here
+def recognize_hinglish_audio(audio_file_path):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(audio_file_path) as source:
+        audio_data = recognizer.record(source)
+        try:
+            text = recognizer.recognize_google(audio_data, language='hi-IN')
+            return text
+        except sr.UnknownValueError:
+            return "Audio not clear"
+        except sr.RequestError:
+            return "Could not request results; check your network connection"
+
+# Example usage
+if __name__ == "__main__":
+    audio_path = "path_to_your_audio_file.wav"
+    recognized_text = recognize_hinglish_audio(audio_path)
+    print("Recognized Text:", recognized_text)
