8ad3299c0de10172d77c982d2075e2c64b220fac
Anubhav Choubey <choubey.anubhav253@gmail.com>
2024-12-20 09:45:30 +0530

    Need to do audio_reco now harder task

--- app.py
+++ app.py
@@ -2,8 +2,3 @@ from utils import add_captions as capt
 from utils import audio_recognition as reco
 
 print("Lets Gooo....")
-
-# Harder Task --- to add something to show current word i am speaking
-
-
-# Lets try with rectangle behind the text
