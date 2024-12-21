b8fa30c990ef672281b7c3ea1556aa6ac577a8d8
Anubhav Choubey <choubey.anubhav253@gmail.com>
2024-12-21 10:33:49 +0530

    Going really well

--- utils/add_captions.py
+++ utils/add_captions.py
@@ -1,6 +1,7 @@
 from moviepy import VideoFileClip, TextClip, CompositeVideoClip
 from typing import List
 
+
 class AddCaptions:
     def __init__(
         self,
@@ -20,7 +21,9 @@ class AddCaptions:
 
     def add_captions(self, output_path: str):
         clips = [self.video]
-        for text, start_time, duration in zip(self.texts, self.start_times, self.durations):
+        for text, start_time, duration in zip(
+            self.texts, self.start_times, self.durations
+        ):
             text_clip = TextClip(
                 font=self.font,
                 text=text,
@@ -32,15 +35,82 @@ class AddCaptions:
                 text_align="center",
             ).with_start(start_time)
             clips.append(text_clip)
-        
-        video_with_text = CompositeVideoClip(clips)
-        video_with_text.write_videofile(output_path, codec="libx264", fps=self.video.fps)
 
-if __name__ == '__main__':
+        video_with_text = CompositeVideoClip(clips)
+        video_with_text.write_videofile(
+            output_path, codec="libx264", fps=self.video.fps
+        )
+
+
+if __name__ == "__main__":
     video_path = "input_video.mp4"
-    texts = ["Sample Caption 1", "Sample Caption 2"]
-    start_times = [0, 5]
-    durations = [2, 3]
+    texts = [
+        "hello",
+        "to",
+        "aaj",
+        "ke",
+        "din",
+        "speech",
+        "recognition",
+        "Nahin",
+        "kam",
+        "Kiya",
+        "Taki",
+        "short",
+        "editing",
+        "Aasan",
+        "ho",
+        "sakeshort",
+        "editing",
+        "Aasan",
+        "ho",
+        "sake",
+        "aur",
+        "to",
+        "Aaj",
+    ]
+    start_times = [
+        0,
+        0.2578,
+        0.5156,
+        0.7733999999999999,
+        1.0312,
+        1.289,
+        1.8445,
+        2.4,
+        2.7236666666666665,
+        3.047333333333333,
+        3.3709999999999996,
+        3.7757142857142854,
+        4.180428571428571,
+        4.5851428571428565,
+        4.989857142857142,
+        5.394571428571427,
+        5.799285714285713,
+        6.203999999999998,
+        6.701499999999998,
+    ]
+    durations = [
+        0.2578,
+        0.2578,
+        0.2578,
+        0.2578,
+        0.2578,
+        0.5555,
+        0.5555,
+        0.32366666666666666,
+        0.32366666666666666,
+        0.32366666666666666,
+        0.40471428571428575,
+        0.40471428571428575,
+        0.40471428571428575,
+        0.40471428571428575,
+        0.40471428571428575,
+        0.40471428571428575,
+        0.40471428571428575,
+        0.4975,
+        0.4975,
+    ]
 
     add_captions = AddCaptions(video_path, texts, start_times, durations)
     add_captions.add_captions("output_video.mp4")
