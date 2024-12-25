f2b1c3832cfe974548efbd7cddf47d8338dd56fc
Anubhav Choubey <choubey.anubhav253@gmail.com>
2024-12-25 10:24:07 +0530

    Video utils added

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from utils import add_captions as capt
 from utils import audio_recognition as reco
 import streamlit as st
+from utils import video_utils
 
 st.title("Video Captioning")
 
 st.write("This is a simple web app to add captions to a video based on the audio of the video.")
 
-st.file_uploader("Upload a Primary Video", type=["mp4"])
-st.file_uploader("Upload a Secondary Video", type=["mp4"])
+primary_video = st.file_uploader("Upload a Primary Video", type=["mp4"])
+print(primary_video)
+secondary_video =  st.file_uploader("Upload a Secondary Video", type=["mp4"])
 
 st.file_uploader("Upload an audio file", type=["wav"])
--- utils/video_utils.py
+++ utils/video_utils.py
@@ -1,21 +1,21 @@
 import os
-from moviepy import AudioFileClip, CompositeAudioClip, CompositeVideoClip
+from moviepy import AudioFileClip, CompositeAudioClip, CompositeVideoClip, VideoFileClip
 
 def remove_audio_from_video(video):
     video_without_audio = video.without_audio()
-
     return video_without_audio
 
 
 def add_audio_to_video(video: CompositeVideoClip, audio_path: str, start_time: float) -> CompositeVideoClip:
     audio = AudioFileClip(audio_path).with_start(start_time)
-    
-    # if video.audio is None:
     video = video.with_audio(audio)
     return video
-    
-    # new_audio = CompositeAudioClip([video.audio, audio])
-    
-    # video_with_new_audio = video.with_audio(new_audio)
 
-    # return video_with_new_audio
+def get_video_duration(video):
+    return video.duration
+
+def get_audio_duration(audio):
+    return audio.duration
+
+def open_video(video_path):
+    return VideoFileClip(video_path)
