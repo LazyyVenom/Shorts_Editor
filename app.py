50adca216fedc0a867efdb1f1600b6ca376e8f7e
Anubhav Choubey <choubey.anubhav253@gmail.com>
2024-12-26 11:03:56 +0530

    Working Great

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from utils import add_captions as capt
 from utils import audio_recognition as reco
+from utils import audio_utils as audio_utils
 import streamlit as st
 from utils import video_utils
 import os
@@ -40,18 +41,26 @@ with st.expander("Secondary Video"):
 
 with st.expander("Audio File"):
     audio_file = st.text_input("Enter the Path of the audio file")
-    audio_file = audio_file.replace('"','')
+    audio_file = audio_file.replace('"', '')
 
     if os.path.exists(audio_file):
-        audio_duration = reco.get_audio_duration(audio_file)
+        audio_duration = audio_utils.get_audio_duration(audio_file)
 
         st.write("Cropping Section for Audio File")
         audio_start_time, audio_end_time = st.slider("Select the start and end time of the audio file", 0.0, audio_duration, (0.0, audio_duration))
         
         if audio_start_time < audio_end_time:
             st.write(f"Selected audio segment from {audio_start_time} to {audio_end_time}")
+            st.audio(audio_file, start_time=audio_start_time, end_time=audio_end_time)
         else:
             st.write("Error: Start time must be less than end time")
 
     else:
-        st.write("The file doesn't exist: ", audio_file)
+        st.write(f"The file doesn't exist: {audio_file}")
+
+
+# Audio Processing Stuff Here
+audio_file = audio_utils.reduce_noise(audio_file, "output.wav")
+
+
+# Video Processing Stuff Here
---
+++ utils/audio_utils.py
@@ -0,0 +1,21 @@
+from scipy.io import wavfile
+import noisereduce as nr
+from pydub import AudioSegment
+
+def reduce_noise(input_file_path, output_file_path):
+    rate, data = wavfile.read(input_file_path)
+    reduced_noise = nr.reduce_noise(y=data, sr=rate)
+    wavfile.write(output_file_path, rate, reduced_noise)
+
+def get_audio_duration(audio_file):
+    rate, data = wavfile.read(audio_file)
+    return len(data) / rate
+
+def audio_speed_increase(input_file_path, output_file_path, speed_factor):
+    if speed_factor < 1.0:
+        raise ValueError("Speed factor must be greater than or equal to 1.0")
+    
+    sound = AudioSegment.from_file(input_file_path)
+    sped_up_sound = sound.speedup(playback_speed=speed_factor, chunk_size=150, crossfade=25)
+    sped_up_sound.export(output_file_path, format="wav")
+    return output_file_path
