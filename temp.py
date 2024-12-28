9fe2b13fd05ef46e6f4fac01329d74b8a5dd22d8
Anubhav Choubey <choubey.anubhav253@gmail.com>
2024-12-28 19:11:25 +0530

    Working ON it

--- temp.py
+++ temp.py
@@ -1,9 +1,9 @@
 from moviepy import VideoFileClip, CompositeVideoClip
 from moviepy.video import fx as vfx
 
-def overlay_transparent_video(original_video, overlay_video_path, color = (), position=("center", "top"), overlay_size=None):
+def overlay_transparent_video(original_video, overlay_video_path, color = (0, 254, 0), position=("center", "top"), overlay_size=None):
     overlay_video = VideoFileClip(overlay_video_path)
-    overlay_video = overlay_video.with_effects([vfx.MaskColor((255, 49, 49),threshold=50,stiffness=100)])
+    overlay_video = overlay_video.with_effects([vfx.MaskColor(color,threshold=50,stiffness=100)])
 
     if overlay_size:
         overlay_video = overlay_video.resized(overlay_size)
--- utils/vfx_utils.py
+++ utils/vfx_utils.py
@@ -1,3 +1,15 @@
 from moviepy import AudioFileClip, CompositeVideoClip, VideoFileClip
 from moviepy.video import fx as vfx
 
+def overlay_transparent_video(original_video, overlay_video_path, color = (0, 254, 0), position=("center", "top"), overlay_size=None):
+    overlay_video = VideoFileClip(overlay_video_path)
+    overlay_video = overlay_video.with_effects([vfx.MaskColor(color,threshold=50,stiffness=100)])
+
+    if overlay_size:
+        overlay_video = overlay_video.resized(overlay_size)
+
+    overlay_video = overlay_video.with_position(position)
+
+    composite_video = CompositeVideoClip([original_video, overlay_video])
+
+    return composite_video
